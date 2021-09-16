2e908de707205f542a8672ad00f4030189ca73c7
EMUNES <hzb000113@qq.com>
2021-09-16 10:03:45 +0800

    vuex added and working

---
+++ src/renderer/store/index.js
@@ -0,0 +1,13 @@
+import { createStore } from 'vuex'
+
+export default createStore({
+  state: {
+    message: "This is the message from vuex",
+  },
+  mutations: {
+  },
+  actions: {
+  },
+  modules: {
+  }
+})
