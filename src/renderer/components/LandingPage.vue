85c2296893b1bc2f4d1927f59e877fd0d4455189
hzb <hzb000113@qq.com>
2021-11-03 14:17:16 +0800

    good

--- src/renderer/components/LandingPage.vue
+++ src/renderer/components/LandingPage.vue
@@ -1,39 +1,42 @@
 <template>
-  <div class="flex py-3 items-center pt-5 w-4/5 m-auto">
-    <input class="w-full px-2 rounded-md focus:outline-none" 
-    type="text" v-model="keyword">
+  <div class="flex py-3 items-center justify-between pt-5 w-4/5 m-auto">
+    <div class="flex items-center justify-center ml-10">
+      <button @click="requestData(true, false)">
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15" />
+        </svg>
+      </button>
+    </div>
 
-    <button class="px-3"
-    @click="onSearch">
-      Search
-    </button>
+    <div class="flex items-center justify-between bg-white rounded-md">
+      <input class="focus:outline-none px-3" @keydown.enter="onSearch"
+      type="text" v-model="keyword">
+
+      <button @click="onSearch">
+        <svg
+        xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+        </svg>
+      </button>
+    </div>
   </div>
 
-  <div class="w-full my-3 px-5">
-    <table class="w-full table-fixed text-left max-w-4xl" v-if="searchResults.length">
-      <colgroup>
-        <col class="w-1/3">
-        <col class="w-1/5">
-        <col>
-      </colgroup>
-
+  <div class="w-4/5 m-auto my-3">
+    <table class="w-full table-fixed text-center max-w-5xl" v-if="searchResults.length">
       <thead>
-        <tr>
-          <th>Image Title</th>
-          <th>Source</th>
+        <tr class="text-xl">
+          <th>News</th>
           <th>Description</th>
         </tr>
       </thead>
       
       <tbody>
         <tr v-for="result in searchResults" v-bind:key="result">
-          <td class="py-6 align-center">
-            <img class="w-4/5 max-w-xs max-h-80 pl-5"
+          <td class="w-1/3 max-w-xs py-6 px-10 align-center">
+            <img class="max-h-80"
             :src="result.urlToImage" alt="Image Unfound">
-            <a class="text-blue-500" :href="result.url">{{ result.title }}</a>
+            <a class="text-blue-500" :href="result.url">{{ result.title }} {{ formatFime(result.publishedAt) }}</a>
           </td>
-
-          <td class="py-6 px-3 align-center">{{ result.author }} {{ formatFime(result.publishedAt) }}</td>
           
           <td class="py-6 align-center">{{ result.description }}</td>
         </tr>
@@ -79,7 +82,6 @@ const onSearch = () => {
     axios.get(SearchUrl)
       .then((res) => {
         searchResults.value = res.data.articles
-        console.log(res)
       })
         .catch((err) => {
           console.log(err)
@@ -102,8 +104,6 @@ const requestData = (store = false, clear = false) => {
         storage.setItem('defaultNews', JSON.stringify(res.data.articles))
       }
 
-      console.log(res.data.articles)
-      console.log(storage.getItem('defaultNews'))
     })
       .catch((err) => {
         console.log(err)
@@ -119,9 +119,7 @@ onMounted(() => {
     } 
     else {
       // Already has, just read.
-      console.log(localData)
       searchResults.value = localData
-      console.log(searchResults.value)
     }
   }
   else {
